130bc3a65bd35a0f926412c709a56235017f11bf
hossein.k <120290053+boredSquidward@users.noreply.github.com>
2024-04-12 00:50:57 -0400

    refac class to className

--- src/components/page/Navbar.jsx
+++ src/components/page/Navbar.jsx
@@ -2,45 +2,46 @@ import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 
 const Navbar = () => (
-  <nav class="bg-gray-800">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-20 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+  <nav className="bg-gray-800">
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="relative flex h-20 items-center justify-between">
+        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <button
             type="button"
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
             aria-controls="mobile-menu"
             aria-expanded="false"
           ></button>
         </div>
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <Link class="flex flex-shrink-0 items-center" to={"/some"}>
-            <img class="h-16 w-auto" src={Logo} alt="Your Company" />
+        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <Link className="flex flex-shrink-0 items-center" to={"/some"}>
+            <img className="h-16 w-auto" src={Logo} alt="Your Company" />
           </Link>
-          <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-4">
+          <div className="hidden sm:ml-6 sm:block">
+            <div className="flex space-x-4">
               <Link
                 href="#"
-                class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
                 aria-current="page"
               >
                 HOME
               </Link>
               <Link
                 href="#"
-                class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                aria-current="page"
               >
                 SERVICES
               </Link>
               <Link
                 href="#"
-                class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
               >
                 CONTACT US
               </Link>
               <Link
                 href="#"
-                class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
               >
                 ABOUT US
               </Link>
@@ -126,30 +127,30 @@ const Navbar = () => (
         </div> */}
       </div>
     </div>
-    <div class="sm:hidden" id="mobile-menu">
-      <div class="space-y-1 px-2 pb-3 pt-2">
+    <div className="sm:hidden" id="mobile-menu">
+      <div className="space-y-1 px-2 pb-3 pt-2">
         <Link
           href="#"
-          class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+          className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
           aria-current="page"
         >
           HOME
         </Link>
         <Link
           href="#"
-          class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
         >
           SERVICES
         </Link>
         <Link
           href="#"
-          class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
         >
           CONTACT US
         </Link>
         <Link
           href="#"
-          class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
         >
           ABOUT US
         </Link>
