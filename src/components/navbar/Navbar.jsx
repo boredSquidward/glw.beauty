e936cc2ef84dfc9e4e9de36198f48cc3d6b0bb86
hossein.k <120290053+boredSquidward@users.noreply.github.com>
2024-04-19 23:15:22 -0400

    update navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,21 +3,28 @@ import { useEffect, useState } from "react";
 
 import Logo from "../../assets/images/logo.png";
 import { appRoutes } from "../../core/constants/routes";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 function Navbar() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const location = useLocation();
 
   const classNameHandler = (path) => {
     if (location.pathname === path) {
       return windowWidth >= 640
-        ? "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium font-sans"
-        : "bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium font-sans";
+        ? "2xl:px-6 px-3 2xl:py-4 py-2 bg-gray-900 text-white 2xl:text-3xl text-sm font-medium rounded-md"
+        : "px-3 py-2 bg-gray-900 text-white block text-base font-medium rounded-md";
     } else {
       return windowWidth >= 640
-        ? "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium font-sans"
-        : "text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium font-sans";
+        ? "2xl:px-6 px-3 2xl:py-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white 2xl:text-3xl text-sm font-medium rounded-md"
+        : "px-3 py-2 text-gray-300 hover:bg-gray-700 hover:text-white block text-base font-medium rounded-md";
     }
+  };
+
+  const clickHandler = () => {
+    setIsDropdownOpen((prevState) => !prevState);
   };
 
   const handleResize = () => {
@@ -34,22 +41,18 @@ function Navbar() {
 
   return (
     <nav className="bg-black sm:fixed w-screen">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-20 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            ></button>
-          </div>
+      <div className="mx-auto w-screen px-2 sm:px-6 lg:px-8">
+        <div className="relative flex 2xl:h-36 h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
             <Link
               className="flex flex-shrink-0 items-center"
               to={appRoutes.homePath}
             >
-              <img className="h-16 w-auto" src={Logo} alt="glw.beauty" />
+              <img
+                className="2xl:h-28 h-16 w-auto"
+                alt="glw.beauty"
+                src={Logo}
+              />
             </Link>
             <div className="hidden sm:ml-6 sm:flex items-center">
               <div className="flex space-x-4">
@@ -83,7 +86,26 @@ function Navbar() {
         </div>
       </div>
       <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
+        <div className="felx flex-row-reverse items-center justify-center w-screen px-2">
+          <Disclosure>
+            <Disclosure.Button
+              onClick={clickHandler}
+              className="p-2 mb-2 relative inline-flex items-center justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            >
+              <span className="absolute -inset-0.5" />
+              {isDropdownOpen ? (
+                <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+              )}
+            </Disclosure.Button>
+          </Disclosure>
+        </div>
+        <div
+          className={`${
+            isDropdownOpen ? "" : "hidden"
+          } space-y-1 px-2 pb-3 pt-2`}
+        >
           <Link
             to={appRoutes.homePath}
             className={classNameHandler(appRoutes.homePath)}
