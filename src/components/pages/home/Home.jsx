188ab4935531417c5ba572e94e916a1f34303d43
hossein.k <120290053+boredSquidward@users.noreply.github.com>
2024-06-21 21:33:44 -0400

    add youtube video

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -11,7 +11,18 @@ const Home = () => {
     <Scaffold>
       <HashLink to="top"></HashLink>
       <TopSection />
-      <div className={`h-screen bg-white flex justify-center items-center`}>
+      <div
+        className={`h-screen bg-white flex flex-col justify-center items-center`}
+      >
+        <iframe
+          height={"80%"}
+          width={"80%"}
+          src={`https://www.youtube.com/embed/U6fC4Ij608A`}
+          frameBorder="0"
+          allow="  accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
         <p>Your video goes here</p>
       </div>
       <ServicesSection />
